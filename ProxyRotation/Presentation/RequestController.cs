5478eb27d852546d32a68b132a7fd15b30cb3816
Z0057393 <Z0057393@protonmail.com>
2025-02-22 15:17:10 +0100

    Alive Check

---
+++ ProxyRotation/Presentation/RequestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProxyRotation.Presentation
+{
+    [ApiController]
+    [Route("/Request")]
+    public class RequestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/alive")]
+        public IActionResult Alive()
+        {
+            return Ok("I'm alive boss");
+        }
+    }
+} 
