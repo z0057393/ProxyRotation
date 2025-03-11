76a3f720c984c0a335de6c128a3e7692a2e062a7
Z0057393 <z0057393@protonmail.com>
2025-03-11 18:33:56 +0100

    Rotate

--- ProxyRotation/04-Domain/Interface/IProxyService.cs
+++ ProxyRotation/04-Domain/Interface/IProxyService.cs
@@ -5,6 +5,6 @@ namespace ProxyRotation.Domain.Interface
     public interface IProxyService
     {
         bool Validate(Proxy proxy);
-        public void Rotate(Proxy proxy, string url);
+        public string Rotate(Proxy proxy, string url);
     }
 }
